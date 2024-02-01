bb059b726624d7b6578d0a74fcf957f88dadbb0a
Ibrahim Aziz <ibraziz21@gmail.com>
2024-02-01 11:20:22 +0300

    Update TGNVault.sol

--- DST-Contracts/contracts/TGNVault.sol
+++ DST-Contracts/contracts/TGNVault.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract SimpleStaking {
+contract TGNVault {
     address private daoContract;  // Address of the DAO contract
     bool private slashingEnabled; 
 
@@ -12,38 +12,46 @@ contract SimpleStaking {
 
     mapping(address => uint256) public stakedBalance;
     mapping(address => uint256) public lastStakedTime;
+    bool private lockStaking;
 
     event Staked(address indexed staker, uint256 amount);
     event Unstaked(address indexed staker, uint256 amount);
     event Slashed(address indexed staker, uint256 amount);
 
+    error InvalidInput();
+    error OnlyDAOAuthorized();
+    error EnableSlashing();
+    error ApproveOrIncreaseAllowance();
+    error InvalidSlashingAmount();
+    error ZeroPercentSlashing();
+
     modifier onlyDAO() {
-        require(msg.sender == daoContract, "Caller is not the DAO contract");
+        if(msg.sender != daoContract) revert OnlyDAOAuthorized();
         _;
     }
 
     modifier slashingAllowed() {
-        require(slashingEnabled, "Slashing is currently disabled");
+        if(!slashingEnabled) revert EnableSlashing();
         _;
     }
 
     constructor(address _tgn, address _DAO) {
-        require(_tgn != address(0), "Invalid token Address");
+        if(_tgn == address(0)) revert InvalidInput();
         daoContract = _DAO;  // Set the DAO contract during deployment
         slashingEnabled = true; 
         tgn = IERC20(_tgn);
     }
 
     function setSlashingParams (uint8 _percent) external  onlyDAO  {
-        require(_percent!=0, "Invalid Input");
+        if(_percent==0) revert InvalidInput();
         slashingPercentage = _percent;
 
     }
 
     // Function to stake tokens
     function stake(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(tgn.allowance(msg.sender, address(this)) >= amount, "Please increase the allowance for this contract");
+        if(amount == 0) revert InvalidInput();
+        if(tgn.allowance(msg.sender, address(this)) < amount) revert ApproveOrIncreaseAllowance();
         tgn.transferFrom(msg.sender, address(this), amount);
 
         stakedBalance[msg.sender] += amount;
@@ -54,8 +62,9 @@ contract SimpleStaking {
 
     // Function to unstake tokens
     function unstake(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
+        if(amount == 0) revert InvalidInput();
         require(stakedBalance[msg.sender] >= amount, "Not enough staked balance");
+        require(lockStaking == false, "Can't unstake till the voting has passed");
 
         tgn.transfer(msg.sender, amount);
 
@@ -67,12 +76,12 @@ contract SimpleStaking {
     // Function to slash a staker's balance (can only be called by the DAO)
   function slash(address staker) external  onlyDAO  slashingAllowed {
     uint256 amount = stakedBalance[staker];
-    require(amount > 0, "Cannot slash zero balance");
+    if (amount== 0) revert InvalidSlashingAmount();
     
-    require(slashingPercentage > 0, "Slashing percentage must be greater than 0");
+    if(slashingPercentage == 0) revert ZeroPercentSlashing();
 
     uint256 slashAmount = (slashingPercentage * amount) / 100;
-    require(slashAmount <= amount, "Slash amount exceeds staked balance");
+    if(slashAmount > amount) revert InvalidSlashingAmount();
 
     stakedBalance[staker] -= slashAmount;
 
@@ -104,11 +113,15 @@ contract SimpleStaking {
     function isSlashingEnabled() external view returns (bool) {
         return slashingEnabled;
     }
+    function setUnstakeLock(bool lock) external {
+        lockStaking = lock;
+    }
 }
 
-interface ISimpleStaking{
+interface ITGNVault{
     function slash(address staker) external;
     function getStakedBalance(address staker) external view returns (uint256); 
     function setSlashingParams (uint8 _percent) external;
+    function setUnstakeLock(bool lock) external returns (bool);
 
 }
